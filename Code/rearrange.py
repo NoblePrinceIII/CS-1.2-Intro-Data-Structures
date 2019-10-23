7d799ca156720929197093241701200c267ed0ac
nobleprinceiii <farhan.begg@students.makeschool.com>
2019-10-23 16:01:10 -0700

    added comment

--- Code/rearrange.py
+++ Code/rearrange.py
@@ -2,17 +2,15 @@ import sys
 import random
 
 mylist=[]
-
+# randomizes my list
 def random_word(mylist):
     random.shuffle(mylist)
     return mylist
 
-
+# adds word to list and then randomizes list
 if __name__ == '__main__':
     for arg in range(1, len(sys.argv)):
         mylist.append(sys.argv[arg])
-
-    print(mylist)
 
     mylist = random_word(mylist)
 
