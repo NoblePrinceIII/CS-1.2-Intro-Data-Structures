d33ef23d258a6a578937137259fd0e3b7f373bba
nobleprinceiii <farhan.begg@students.makeschool.com>
2019-10-27 21:50:41 -0700

    removed shuffle

--- Code/rearrange.py
+++ Code/rearrange.py
@@ -2,17 +2,14 @@ import random
 import sys
 
 def rearrange(words_list):
-    """
-    rearranges given words from the user
-    """
+    # rearrange given word
     for i, word in enumerate(words_list):
         random_index = random.randint(0, len(words_list) - 1)
         words_list[i] = words_list[random_index]
         words_list[random_index] = word
 
     return words_list
-    # random.shuffle(word_list)
-    # return word_list
+    # return word_list 
 
 if __name__ == '__main__':
     word_list = sys.argv[1:]
