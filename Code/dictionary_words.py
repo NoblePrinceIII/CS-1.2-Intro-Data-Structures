2bd331c8173e41cac58ed273ca5a439c365f4972
nobleprinceiii <farhan.begg@students.makeschool.com>
2019-10-27 22:01:03 -0700

    fixed rearrange

--- Code/dictionary_words.py
+++ Code/dictionary_words.py
@@ -1,5 +1,5 @@
 import sys
-import random
+from random import choice
 
 
 # opens text file from location 
@@ -7,8 +7,19 @@ f = open("/usr/share/dict/words", "r")
 
 word_list=f.readlines()
 
-def random_word():
+def random_word(amount):
     # Checks words in list and prints randomize words in range
-    for i in range(int(sys.argv[1])):
-        print(word_list[random.randint(0, len(word_list-1))] ) 
+    count = 0
+    random = []
+    f.close()
 
+    while count < int(amount):
+        random.append(choice(word_list).strip())
+        count +=1
+    return ' '.join(random)
+
+
+if __name__ == "__main__":
+    amount = sys.argv[1]
+    output = random_word(amount)
+    print(output)
