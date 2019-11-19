64c52992ef905fe0502795bbc54298bb78d64d87
NoblePrinceii <farhan.begg@students.makeschool.com>
2019-11-19 15:13:20 -0800

    finished linkedlist.py

--- Code/markov.py
+++ Code/markov.py
@@ -1,29 +1,29 @@
-from dictogram import Dictogram
-import random 
-from histogram import open_file
+# from dictogram import Dictogram
+# import random 
+# from histogram import open_file
 
-def open_file(words_file):
-    '''Opens text file and arranges words into a readable list '''    
+# def open_file(words_file):
+#     '''Opens text file and arranges words into a readable list '''    
 
-    with open (words_file, 'r') as f:
-        words = f.read()
-        scrubbed_words = re.sub(r'[^a-zA-Z\s]', '', words)
+#     with open (words_file, 'r') as f:
+#         words = f.read()
+#         scrubbed_words = re.sub(r'[^a-zA-Z\s]', '', words)
     
-    return scrubbed_words.split(" ")
+#     return scrubbed_words.split(" ")
 
 
 
-def generate_sentence(chain, count=15):
+# def generate_sentence(chain, count=15):
 
-    word1 = random.choice(list(chain.keys()))
+#     word1 = random.choice(list(chain.keys()))
     
-    for i in range(count-1):
-        word2 = random.choice(chain[word1])
-        word1 = word2
-        sentence += ' ' + word2
+#     for i in range(count-1):
+#         word2 = random.choice(chain[word1])
+#         word1 = word2
+#         sentence += ' ' + word2
 
-    return(sentence )
+#     return(sentence )
 
-words_file = ('test.txt')
-generate_sentence(ali_dict)
+# words_file = ('test.txt')
+# generate_sentence(ali_dict)
 
